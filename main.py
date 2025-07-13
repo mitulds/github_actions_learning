61a276f6423df4a841cf300e648fcbde960d7e25
mitulds <120341649+mitulds@users.noreply.github.com>
2025-07-13 17:08:41 -0600

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,19 @@ def load_sales_data(path="sales.csv"):
     """
     df = pd.read_csv(path).drop(columns=["Unnamed: 0"])
     return df
+
+
+def total_sales(df):
+    return df["total"].sum()
+
+
+def unique_customers(df):
+    return df["customer_type"].nunique()
+
+
+def average_price(df):
+    return round(df["unit_price"].mean(), 2)
+
+
+def get_by_category(df, category):
+    return df[df["category"] == category]
